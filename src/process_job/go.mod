0929357a5b0b56eb966661c86d28444eee66eaf6
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2025-05-21 13:29:59 +0530

    fs-listener for transcoded HLS segments (process_job)

--- src/process_job/go.mod
+++ src/process_job/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/envoyproxy/go-control-plane/envoy v1.32.4 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.2.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -26,6 +27,7 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	github.com/spiffe/go-spiffe/v2 v2.5.0 // indirect
 	github.com/zeebo/errs v1.4.0 // indirect
--- src/process_job/main.go
+++ src/process_job/main.go
@@ -4,24 +4,61 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"cloud.google.com/go/storage"
+	"github.com/fsnotify/fsnotify"
+	"github.com/joho/godotenv"
 	"google.golang.org/api/iterator"
 )
 
-const bucketName string = `temp-bucket-d08bdb88`
+var HLSbucket string
 
 // region methods
 // ====================
 
 func main() {
+	godotenv.Load()
+	HLSbucket = os.Getenv("HLS_BUCKETNAME")
+
 	ctx := context.Background()
 	cli, err := storage.NewClient(ctx)
 	defer cli.Close()
 	checkErr(err)
-	bkt := cli.Bucket(bucketName)
+	// bkt := cli.Bucket(HLSbucket)
 
-	listGCS(ctx, bkt)
+	//worker-coroutines (background offloads)
+	watcher, err := fsnotify.NewWatcher()
+	checkErr(err)
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event.Name, " ", event.Op)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("./out/stream_0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-make(chan struct{})
+}
+
+func worker_routine(ctx context.Context, bkt *storage.BucketHandle, streamDir string) {
 }
 
 // list gcs objects in bucket
@@ -38,8 +75,22 @@ func listGCS(ctx context.Context, bkt *storage.BucketHandle) {
 }
 
 // upload file from local to gcs bucket
-func uploadGCS(bkt *storage.BucketHandle, filepath string) {
+func uploadGCS(ctx context.Context, bkt *storage.BucketHandle, file string) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		log.Printf("read error: %v", err)
+		return
+	}
+	obj := bkt.Object(filepath.Base(file))
+	w := obj.NewWriter(ctx)
+	w.ChunkSize = 0 // no chunk upload (for small files)
 
+	if _, err := w.Write(data); err != nil {
+		log.Printf("upload error: %v", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Printf("writer close error: %v", err)
+	}
 }
 
 // region utils
