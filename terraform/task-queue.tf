936a499673bf2cec5bb687a9cb8f6f7afb3129e4
Shubham Tiwary <shubhamtiwary914@gmail.com>
2025-06-03 18:22:41 +0530

    task queue for rate limit (set=20) -> terraform cf

--- terraform/task-queue.tf
+++ terraform/task-queue.tf
@@ -1,23 +1,39 @@
 
 
-# resource "google_cloud_tasks_queue" "simple" {
-#   name = "buffer"
-#   location = var.region_queue
+resource "google_cloud_tasks_queue" "simple" {
+  name = "build-queue"
+  location = var.region_queue
 
-#   http_target {
-#     uri_override {
-#         scheme = "HTTPS"
-#         host = "587f-2401-4900-5f75-4ee3-54dd-9ede-e8f6-c381.ngrok-free.app" 
-#         path_override {
-#           path = "/queue-hook"
-#         }
-#     }
-#   }
-# }
+    http_target {
+        uri_override {
+            scheme = "HTTPS"
+            host = "job-runner-service-${var.project_number}.${var.region}.run.app" 
+            path_override {
+              path = "/job-run"
+            }
+        }
+        http_method = "POST"
+        header_overrides {
+          header {
+            key = "Content-Type"
+            value = "application/json"
+          }
+        }
+
+        oidc_token {
+          service_account_email = var.user_SA
+        }
+    }
+
+    rate_limits {
+      max_concurrent_dispatches = 20
+      max_dispatches_per_second = 5
+    }
+}
 
 
-# resource "google_project_iam_member" "enqueuer" {
-#     project = var.project_id
-#     role   = "roles/cloudtasks.enqueuer"
-#     member = "serviceAccount:${var.user_SA}"
-# }
+resource "google_project_iam_member" "enqueuer" {
+    project = var.project_id
+    role   = "roles/cloudtasks.enqueuer"
+    member = "serviceAccount:${var.user_SA}"
+}
